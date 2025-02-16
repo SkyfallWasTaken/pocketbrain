be6f1c005853fed5804e28b28673b970b1514abb
Mahad Kalam <mahadkalam1@proton.me>
2025-02-16 10:10:15 +0000

    Make it use Clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 mod machine;
 mod parser;
+use clap::Parser;
 use machine::Machine;
+use std::fs;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the Brainfuck source file
+    file: String,
+}
 
 fn main() {
-    let mut machine = Machine::from_input(include_str!("../test.bf").to_string());
+    let args = Args::parse();
+    let source = fs::read_to_string(args.file).expect("Failed to read the source file");
+
+    let mut machine = Machine::from_input(source);
     machine.execute();
-    // println!("{:?}", instrs);
 }
